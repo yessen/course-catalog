c982abc76217928d5e7146b08e0e6899af5b3b23
brilianvy2 <brilianvincenza8@gmail.com>
2025-02-17 21:34:09 +0700

    populating models

--- course_catalog_app/models.py
+++ course_catalog_app/models.py
@@ -14,7 +14,7 @@ class Course(models.Model):
     course_name = models.CharField(max_length=255)
     scu = models.IntegerField()
     is_core = models.BooleanField()
-    passing_grade = models.CharField(max_length=2, null=True, blank=True)
+    passing_grade = models.CharField(max_length=2, null=True, blank=True, default='NA')
     prerequisites = models.TextField(null=True, blank=True)
 
     def __str__(self):
@@ -26,3 +26,20 @@ class SemesterCourse(models.Model):
 
     class Meta:
         unique_together = ('semester', 'course')
+
+class LearningObjective(models.Model):
+    code = models.CharField(max_length=10, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.code
+
+class AssessmentPlan(models.Model):
+    learning_objective = models.ForeignKey(LearningObjective, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    assessment_method = models.CharField(max_length=100)
+    weight = models.IntegerField()
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.learning_objective.code} - {self.course.course_name} ({self.semester.semester_no})"
