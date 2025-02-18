7ed75a69076b00314595e17b80d04ff3a72c732d
brilianvy2 <brilianvincenza8@gmail.com>
2025-02-18 15:40:10 +0700

    frontend

--- frontend-app/src/App.jsx
+++ frontend-app/src/App.jsx
@@ -1,33 +1,95 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Binus Course Catalogue</h1>
+      <div className="row">
+        <div className= "column" >
+          {/* This table is for the Semester */}
+          <table className="scrolldown">
+            <tr>
+              <th>Semester No.</th>
+              <th>Total SCU</th>
+              <th>Optional Description</th>
+            </tr>
+            <tr>
+              <td>Content</td>
+              <td>No</td>
+              <td>Content</td>
+            </tr>
+            <tr>
+              <td>Content</td>
+              <td>No</td>
+              <td>Content</td>
+            </tr>
+            <tr>
+            <td>Content</td>
+              <td>No</td>
+              <td>Content</td>
+            </tr>
+            <tr>
+              <td>Content</td>
+              <td>No</td>
+              <td>Content</td>
+            </tr>
+            <tr>
+              <td>Content</td>
+              <td>No</td>
+              <td>Content</td>
+            </tr>
+            <tr>
+              <td>Content</td>
+              <td>No</td>
+              <td>Content</td>
+            </tr>
+            <tr>
+            <td>Content</td>
+              <td>No</td>
+              <td>Content</td>
+            </tr>
+          </table>
+        </div>
+        {/* Table for Course List */}
+        <div className= "column" >
+          <table>
+            <tr>
+              <th>Course List</th>
+              <th>SCU</th>
+            </tr>
+            <tr>
+              <td>Content</td>
+              <td>No</td>
+            </tr>
+            <tr>
+              <td>Content</td>
+              <td>No</td>
+            </tr>
+          </table>
+        </div>
+        {/* Table for course and Semester */}
+        <div className= "column" >
+          <table>
+            <tr>
+              <th>Semester No</th>
+              <th>Course Name</th>
+            </tr>
+            <tr>
+              <td>No</td>
+              <td>Name</td>
+            </tr>
+            <tr>
+              <td>No</td>
+              <td>Name</td>
+            </tr>
+            <tr>
+              <td>No</td>
+              <td>Name</td>
+            </tr>
+          </table>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
