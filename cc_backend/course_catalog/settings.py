79c6c9972ac3148e7db2153d06f513ba9ef7b746
Zhandos Yessenbayev <yessen@mail.ru>
2025-02-25 19:16:23 +0700

    posgres

--- cc_backend/course_catalog/settings.py
+++ cc_backend/course_catalog/settings.py
@@ -152,3 +152,4 @@ STATIC_ROOT = BASE_DIR / "staticfiles" / "static"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
