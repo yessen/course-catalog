79e51d2c5cc5ff13f3a1588ddc5d75454c2bd88b
Zhandos Yessenbayev <yessen@mail.ru>
2025-02-25 15:03:46 +0700

    .gitignore

--- course_catalog_app/models.py
+++ course_catalog_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import re
 
 class Semester(models.Model):
     semester_no = models.IntegerField()
@@ -24,5 +25,23 @@ class SemesterCourse(models.Model):
     semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
-    class Meta:
-        unique_together = ('semester', 'course')
+    def __str__(self):
+        return f"Semester {self.semester.semester_no}: {self.course.course_name} ({self.course.course_code})"
+
+
+class LearningObjective(models.Model):
+    code = models.CharField(max_length=10, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.code
+
+class AssessmentPlan(models.Model):
+    learning_objective = models.ForeignKey(LearningObjective, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    assessment_method = models.CharField(max_length=100)
+    weight = models.IntegerField()
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.learning_objective.code} - {self.course.course_name} ({self.semester.semester_no})"
--- course_catalog_app/views.py
+++ course_catalog_app/views.py
@@ -2,6 +2,10 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import Semester, Course, SemesterCourse
 from .serializers import SemesterSerializer, CourseSerializer, SemesterCourseSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 class SemesterViewSet(viewsets.ModelViewSet):
     queryset = Semester.objects.all()
