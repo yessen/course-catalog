de904f1154d9c8ca28b57ede9a6bb5e122c6c29b
Zhandos Yessenbayev <yessen@mail.ru>
2025-02-25 17:08:07 +0700

    vercel

--- cc_backend/install.sh
+++ cc_backend/install.sh
@@ -1,6 +1,7 @@
-mkdir env
-python3 -m venv env
-source env/bin/activate
-
+python3 -m venv sc-env
+source sc-env/bin/activate
 pip install -r requirements.txt
-python manage.py runserver
+
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic --noinput
