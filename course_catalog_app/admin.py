3fb6677cdc8899e07d53d4d91a71a0063ed9bdb1
Zhandos Yessenbayev <yessen@mail.ru>
2025-02-25 15:32:36 +0700

    delete all

--- course_catalog_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Semester, Course, SemesterCourse
-
-admin.site.register(Semester)
-admin.site.register(Course)
-admin.site.register(SemesterCourse)
